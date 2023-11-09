e315ed437c34a974b10a3d7ea6e608ef07d2c0bb
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-11-10 01:59:43 +0800

    style: refactored act12 code

--- src/act12.cpp
+++ src/act12.cpp
@@ -1,20 +1,21 @@
 #include <curses.h>
 
+const int MAX_INPUT = 10;
+
 int main()
 {
+  int input[MAX_INPUT];
+
   initscr();
-
-  int input[10];
-
-  mvprintw(0, 0, "Enter the age of 10 people:");
-  for (int i = 0; i < 10; i++) {
+  mvprintw(0, 0, "Enter the age of %i people:", MAX_INPUT);
+  for (int i = 0; i < MAX_INPUT; i++) {
     mvprintw(i+1, 2, "%d%s", i+1, ". ");
     refresh();
     scanw("%d", &input[i]);
   }
 
   mvprintw(0, 0, "Given your input, the following are:");
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < MAX_INPUT; i++) {
     if (input[i] >= 18)
       mvprintw(i+1, 10, "18+");
     else
