03b26a594776b7676c768346c5f7471c4a72681b
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-11-10 02:32:43 +0800

    refactor: compile-time constants usage

--- src/act12.cpp
+++ src/act12.cpp
@@ -1,6 +1,7 @@
 #include <curses.h>
 
 const int MAX_INPUT = 10;
+const int AGE_LEGAL = 18;
 
 int main()
 {
@@ -9,17 +10,17 @@ int main()
   initscr();
   mvprintw(0, 0, "Enter the age of %i people:", MAX_INPUT);
   for (int i = 0; i < MAX_INPUT; i++) {
-    mvprintw(i+1, 2, "%d%s", i+1, ". ");
+    mvprintw(i + 1, 2, "%d. ", i + 1);
     refresh();
     scanw("%d", &input[i]);
   }
 
   mvprintw(0, 0, "Given your input, the following are:");
   for (int i = 0; i < MAX_INPUT; i++) {
-    if (input[i] >= 18)
-      mvprintw(i+1, 10, "18+");
+    if (input[i] >= AGE_LEGAL)
+      mvprintw(i + 1, 10, "- of legal age");
     else
-      mvprintw(i+1, 10, "under 18");
+      mvprintw(i + 1, 10, "- under %i", AGE_LEGAL);
   }
   refresh();
 
