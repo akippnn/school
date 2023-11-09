a076afe52a0147d3eed4a15da757eab6ac45a059
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-11-09 13:30:41 +0800

    fix: missing semicolons and unused declarations

--- src/act12.cpp
+++ src/act12.cpp
@@ -1,9 +1,7 @@
 #include <curses.h>
 
-using std::cout;
-using std::cin;
-
-int main() {
+int main()
+{
   initscr();
   curs_set(0);
 
@@ -13,15 +11,15 @@ int main() {
   for (int i = 0; i < 10; i++) {
     mvprintw(i+1, 2, "%d%s", i+1, ". ");
     refresh();
-    scanw("%d", &input[i])
+    scanw("%d", &input[i]);
   }
 
   mvprintw(0, 0, "Given your input, the following are:");
   for (int i = 0; i < 10; i++) {
     if (input[i] >= 18)
-      mvprintw(i+1, 10, "18+")
+      mvprintw(i+1, 10, "18+");
     else
-      mvprintw(i+1, 10, "under 18")
+      mvprintw(i+1, 10, "under 18");
   }
   refresh();
 
