72ac1da2af742eb315c6e96e903b04474616b9d7
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-11-10 03:19:25 +0800

    refactor: style + compile-time constants

--- src/act21.cpp
+++ src/act21.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 
-using std::cout;
 using std::cin;
+using std::cout;
 
-int main() {
-  int num[5][5];
+const int NUM_LIMIT = 100;
+const int X_SIZE = 5;
+const int Y_SIZE = 5;
+
+int main()
+{
+  int num[X_SIZE][Y_SIZE];
   int input;
-  for (int y = 0; y < 5; y++) {
-    for (int x = 0; x < 5; x++) {
-      while(true) {
-        cout << "Enter for (" << y << ", " << x << "): ";
-        cin >> input;
-        if (input > 100) {
-          cout << "Please input a number less than 100.\n";
-        } else {
-          num[x][y] = input;
-          break;
-        }
+
+  for (int y = 0; y < Y_SIZE; y++) {
+    for (int x = 0; x < X_SIZE; x++) {
+      cout << "Enter for (" << y << ", " << x << "): ";
+
+      cin >> input;
+      if (input > NUM_LIMIT) {
+        cout << "Please input a number less than 100.\n";
+        x--;
+      }
+      else {
+        num[x][y] = input;
+      }
+
+      if (x < 0) {
+        y--;
+        x++;
       }
     }
   }
 
   cout << "Your newly created table:\n";
-  for (int y = 0; y < 5; y++) {
-    for (int x = 0; x < 5; x++) {
+  for (int y = 0; y < Y_SIZE; y++) {
+    for (int x = 0; x < X_SIZE; x++) {
       cout << "\t" << num[x][y];
     }
     cout << "\n";
   }
+  
+  return 0;
 }
