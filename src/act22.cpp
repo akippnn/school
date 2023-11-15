3bc44a8d2aae043560b9a0949c10b48185367a3b
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-11-16 03:07:00 +0800

    feat: initial work for act22 to use ncurses

--- src/act22.cpp
+++ src/act22.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
 #include <ncurses.h>
 
-using std::cout;
-using std::cin;
+int printTable(int num[])
 
-int main() {
+int main()
+{
   int num[5][5] = {
-    10,  12,  14, 16, 18,
-    20,  55,  75, 65, 85,
-    95, 200, 150, 65, 34,
-    90,  91,  87, 60, 63,
-    23,  19,  11, 34, 54
+    10,   12,   14,   16,   18,
+    20,   55,   75,   65,   85,
+    95,   200,  150,  65,   34,
+    90,   91,   87,   60,   63,
+    23,   19,   11,   34,   54
   };
   int row, column;
   char input;
-  cout << "These are the numbers in the table:\n\t1.\t2.\t3.\t4.\t5.\tcolumn\n";
-  for (int y = 0; y < 5; y++) {
-    cout << y + 1 << ".";
-    for (int x = 0; x < 5; x++) {
-      cout << "\t" << num[x][y];
-    };
-    cout << "\n";
-  };
-  cout << "row\n";
 
-  cout << "Row []";
+  initscr();
+  printw("These are the numbers in the table:");
+  mvprintw(y+2, 0, "%i", y);
+  for (int y = 0; y < 5; y++) {
+    mvprintw(y+2, 0, "%i", y);
+    for (int x = 0; x < 5; x++) {
+      mvprintw(y+2, 2*x, "%i", num[x][y]);
+    };
+  };
+  refresh();
+
+  mvprintw(10, 0, "Row [ Type number... ]");
+  refresh();
   input = getch();
   row = int(input) - 48;
-  cout << "\b" << row << "] ";
+  mvprintw(10, 0, "Row [%i]", row);
 
-  cout << "Column []";
+  mvprintw(1)
+  refresh();
   input = getch();
   column = int(input) - 48;
   cout << "\b" << column << "] ";
-  
+
   cout << "\nThe value in the field you selected (" << row << ", " << column << ") is: " << num[column - 1][row - 1];
 
   return 0;
